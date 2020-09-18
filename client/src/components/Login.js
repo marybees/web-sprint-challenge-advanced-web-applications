9ec91b0902702aa09055c97e9ac4870d8e21f0e1
Mary Baker <meb601@gmail.com>
2020-09-18 11:52:45 -0400

    Build private route and login page

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,13 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
+
+const credentials = {
+  username: "",
+  password: "",
+};
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const [login, setLogin] = useState(credentials);
+
+  let history = useHistory();
+
+  const handleChange = (e) => {
+    setLogin({ ...login, [e.target.name]: e.target.value });
+  };
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    axiosWithAuth()
+      .post("/api/login", login)
+      .then((res) => {
+        console.log(res.data.payload);
+        localStorage.setItem("token", res.data.payload);
+        history.push("/colors");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleLogin}>
+        <p>
+          <label>
+            Username:&nbsp;
+            <input
+              type="text"
+              name="username"
+              placeholder="Username"
+              value={login.username}
+              onChange={handleChange}
+            />
+          </label>
+        </p>
+        <p>
+          <label>
+            Password:&nbsp;
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              value={login.password}
+              onChange={handleChange}
+            />
+          </label>
+        </p>
+        <button>Login</button>
+      </form>
+    </div>
   );
 };
 
